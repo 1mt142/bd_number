aef16aed7724715ded9e46b1513f87e3837eb7ee
Mohsin Khandoker <mohsin.khondoker@upaybd.com>
2021-12-30 16:48:49 +0600

    bnToEnNum

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 #This package is for bd_number system
+
+const {bnToEnNum} = require('bd_number');
+var bn = bnToEnNum('৮৯৭৪২৩৬৩৬৯৬');
+console.log(banglaNumber); // 89742363696
--- index.js
+++ index.js
@@ -1,4 +1,33 @@
-//This function will print the current date
-exports.printDate = function () {
-  console.log("Current Date is", new Date());
-};
+/**
+ * @param {String} banglaNumber
+ * @return {number}
+ */
+
+function bnToEnNum(banglaNumber = "") {
+  if (typeof banglaNumber !== "string") {
+    throw {
+      message: `${banglaNumber} must be string and Bangla number`,
+    };
+  }
+  String.prototype.gValue = function () {
+    const numStore = {
+      "০": "0",
+      "১": "1",
+      "২": "2",
+      "৩": "3",
+      "৪": "4",
+      "৫": "5",
+      "৬": "6",
+      "৭": "7",
+      "৮": "8",
+      "৯": "9",
+    };
+    let gx = this;
+    for (let x in numStore) {
+      gx = gx.replace(new RegExp(x, "g"), numStore[x]);
+    }
+    return gx;
+  };
+  return banglaNumber.gValue();
+}
+module.exports = { bnToEnNum };
