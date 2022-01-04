3a642ebb392111146aa3b9de736d30d3bcc151ce
Mohsin Khandoker <mohsin.khondoker@upaybd.com>
2022-01-04 16:47:47 +0600

    words

--- index.js
+++ index.js
@@ -1,38 +1,5 @@
 /**
- * @param {String} banglaNumber
- * @return {number}
- */
-
-function bnToEnNum(banglaNumber = "") {
-  if (typeof banglaNumber !== "string") {
-    throw {
-      message: `${banglaNumber} must be string and Bangla number`,
-    };
-  }
-  String.prototype.gValue = function () {
-    const numStore = {
-      "০": "0",
-      "১": "1",
-      "২": "2",
-      "৩": "3",
-      "৪": "4",
-      "৫": "5",
-      "৬": "6",
-      "৭": "7",
-      "৮": "8",
-      "৯": "9",
-    };
-    let gx = this;
-    for (let x in numStore) {
-      gx = gx.replace(new RegExp(x, "g"), numStore[x]);
-    }
-    return gx;
-  };
-  return banglaNumber.gValue();
-}
-
-/**
- * @param {Number} banglaAmount
+ * @param {Number} amount
  * @return {String}
  */
 
@@ -238,4 +205,4 @@ function toWord(number, words) {
   }
   return words_string;
 }
-module.exports = { bnToEnNum, enToBnWord };
+module.exports = { enToBnWord };
